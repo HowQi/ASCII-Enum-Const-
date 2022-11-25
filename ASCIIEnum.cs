52fa485ad4c54d7b091a222d07e7d5ba84584e7a
How Qimiu <expectmiu@yeah.net>
2022-11-25 21:48:01 +0800

    Update All
    It's all.

---
+++ ASCIIEnum.cs
@@ -0,0 +1,166 @@
+﻿
+
+public static class ASCIIEnum
+{
+    public const char NUL = '\x00';
+    public const char SOH = '\x01';
+    public const char STX = '\x02';
+    public const char ETX = '\x03';
+
+    public const char EOT = '\x04';
+    public const char ENQ = '\x05';
+    public const char ACK = '\x06';
+    public const char BEL = '\x07';
+
+    public const char BS = '\x08';
+    public const char HT = '\x09';
+    public const char LF = '\x0a';
+    public const char VT = '\x0b';
+
+    public const char FF = '\x0c';
+    public const char CR = '\x0d';
+    public const char SO = '\x0e';
+    public const char SI = '\x0f';
+
+    public const char DLE = '\x10';
+    public const char DC1 = '\x11';
+    public const char DC2 = '\x12';
+    public const char DC3 = '\x13';
+
+    public const char DC4 = '\x14';
+    public const char NAK = '\x15';
+    public const char SYN = '\x16';
+    public const char ETB = '\x17';
+
+    public const char CAN = '\x18';
+    public const char EM = '\x19';
+    public const char SUB = '\x1a';
+    public const char ESC = '\x1b';
+
+    public const char FS = '\x1c';
+    public const char GS = '\x1d';
+    public const char RS = '\x1e';
+    public const char US = '\x1f';
+
+    public const char SP = '\x20';
+    public const char SignExclamation = '\x21';
+    public const char SignDoubleQuote = '\x22';
+    public const char SignOfNumber = '\x23';
+
+    public const char SignOfDollar = '\x24';
+    public const char SignPercent = '\x25';
+    public const char SignAmpersand = '\x26';
+    public const char SignSingleQuote = '\x27';
+
+    public const char SignLeftParenthesis = '\x28';
+    public const char SignRightParenthesis = '\x29';
+    public const char SignAsterisk = '\x2a';
+    public const char SignPlus = '\x2b';
+
+    public const char SignComma = '\x2c';
+    public const char SignMinus = '\x2d';
+    public const char SignPeriod = '\x2e';
+    public const char SignSlash = '\x2f';
+
+    public const char Number0 = '\x30';
+    public const char Number1 = '\x31';
+    public const char Number2 = '\x32';
+    public const char Number3 = '\x33';
+
+    public const char Number4 = '\x34';
+    public const char Number5 = '\x35';
+    public const char Number6 = '\x36';
+    public const char Number7 = '\x37';
+
+    public const char Number8 = '\x38';
+    public const char Number9 = '\x39';
+    public const char SignColon = '\x3a';
+    public const char SignSemicolon = '\x3b';
+
+    public const char SignLessThan = '\x3c';
+    public const char SignEqualitySign = '\x3d';
+    public const char SignGreaterThan = '\x3e';
+    public const char SignQuestionMark = '\x3f';
+
+    public const char SignOfAt = '\x40';
+    public const char CapitalA = '\x41';
+    public const char CapitalB = '\x42';
+    public const char CapitalC = '\x43';
+
+    public const char CapitalD = '\x44';
+    public const char CapitalE = '\x45';
+    public const char CapitalF = '\x46';
+    public const char CapitalG = '\x47';
+
+    public const char CapitalH = '\x48';
+    public const char CapitalI = '\x49';
+    public const char CapitalJ = '\x4a';
+    public const char CapitalK = '\x4b';
+
+    public const char CapitalL = '\x4c';
+    public const char CapitalM = '\x4d';
+    public const char CapitalN = '\x4e';
+    public const char CapitalO = '\x4f';
+
+    public const char CapitalP = '\x50';
+    public const char CapitalQ = '\x51';
+    public const char CapitalR = '\x52';
+    public const char CapitalS = '\x53';
+
+    public const char CapitalT = '\x54';
+    public const char CapitalU = '\x55';
+    public const char CapitalV = '\x56';
+    public const char CapitalW = '\x57';
+
+    public const char CapitalX = '\x58';
+    public const char CapitalY = '\x59';
+    public const char CapitalZ = '\x5a';
+    public const char SignLeftSquareBracket = '\x5b';
+
+    public const char SignBackslash = '\x5c';
+    public const char SignRightSquareBracket = '\x5d';
+    public const char SignCaret = '\x5e';
+    public const char SignUnderscore = '\x5f';
+
+    public const char SignGrave = '\x60';
+    public const char SmallA = '\x61';
+    public const char SmallB = '\x62';
+    public const char SmallC = '\x63';
+
+    public const char SmallD = '\x64';
+    public const char SmallE = '\x65';
+    public const char SmallF = '\x66';
+    public const char SmallG = '\x67';
+
+    public const char SmallH = '\x68';
+    public const char SmallI = '\x69';
+    public const char SmallJ = '\x6a';
+    public const char SmallK = '\x6b';
+
+    public const char SmallL = '\x6c';
+    public const char SmallM = '\x6d';
+    public const char SmallN = '\x6e';
+    public const char SmallO = '\x6f';
+
+    public const char SmallP = '\x70';
+    public const char SmallQ = '\x71';
+    public const char SmallR = '\x72';
+    public const char SmallS = '\x73';
+
+    public const char SmallT = '\x74';
+    public const char SmallU = '\x75';
+    public const char SmallV = '\x76';
+    public const char SmallW = '\x77';
+
+    public const char SmallX = '\x78';
+    public const char SmallY = '\x79';
+    public const char SmallZ = '\x7a';
+    public const char SignLeftCurlyBracket = '\x7b';
+
+    public const char SignVerticalBar = '\x7c';
+    public const char SignRightCurlyBracket = '\x7d';
+    public const char SignTilde = '\x7e';
+    public const char DEL = '\x7f';
+
+}
+
